320144521748f9b9fdc02b2921bbe919564219fc
liamwe <liam.millar@webelevate.ie>
2016-07-06 19:03:44 +0100

    Add files via upload

--- myController.js
+++ myController.js
@@ -1,13 +1,15 @@
 var app= angular.module('myApp',[]);
 app.controller('todoCtrl',function($scope){
+	/* empty list */
 	$scope.todoList = [];
 	
+	/* Add Items to the list function then resetting the inputs to blank*/
 	$scope.todoAdd = function(){
 		$scope.todoList.push({todoText:$scope.todoInput.name,todoDesc:$scope.todoInput.description});
 		$scope.todoInput.name="";
 		$scope.todoInput.description="";
 		};
-	
+	/* Delete Items from the list by taking the Index and removing one Item at that point*/
 	 $scope.delete = function(x){
 	 $scope.todoList.splice(x,1);
 	 };
